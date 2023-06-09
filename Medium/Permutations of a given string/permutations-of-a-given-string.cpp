62e0d698aefe9e4b8a2af794e2ec9530e31a22db
Cypher <debangshudas200@gmail.com>
2023-06-09 12:04:42 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Permutations of a given string/permutations-of-a-given-string.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+		vector<string>find_permutation(string s)
+{
+	vector <string> ve;
+        sort(s.begin(), s.end());
+       do
+       {
+		ve.push_back(s);	                    
+        }while (next_permutation(s.begin(), s.end()));    
+        return ve;
+}	
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--)
+    {
+	    string S;
+	    cin >> S;
+	    Solution ob;
+	    vector<string> ans = ob.find_permutation(S);
+	    for(auto i: ans)
+	    {
+	    	cout<<i<<" ";
+	    }
+	    cout<<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
