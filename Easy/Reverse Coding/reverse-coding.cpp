a38e1bf72df2af449c81b41bcf1a70ad80cc7020
Cypher <debangshudas200@gmail.com>
2023-06-21 11:34:28 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse Coding/reverse-coding.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int sumOfNaturals(int n) {
+        // code here
+         long long m=1000000007;
+        long long sum=(n*((n+1)%m)/2)%m;
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        
+        cin>>n;
+
+        Solution ob;
+        cout << ob.sumOfNaturals(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
