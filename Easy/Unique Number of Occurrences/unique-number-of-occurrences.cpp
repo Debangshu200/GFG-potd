d2670d1f736e2aba68061c8c82cf42e4ea1ffca4
Cypher <debangshudas200@gmail.com>
2023-07-13 13:07:41 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Unique Number of Occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    bool isFrequencyUnique(int n, int arr[])
+    {
+      map<int,int> m;
+      
+      
+      for(int i=0;i<n;i++){
+          m[arr[i]]++;
+      }
+      
+      int c = m[arr[0]];
+      
+      
+      
+      for(auto i:m){
+         
+         if(arr[i.second -1 ] == -999) 
+         return false;
+         
+         arr[i.second - 1 ] = -999;
+         
+      }
+      
+      
+      return true;
+      
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        bool ans=ob.isFrequencyUnique(n,arr);
+        if(ans)
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+}
+// } Driver Code Ends
